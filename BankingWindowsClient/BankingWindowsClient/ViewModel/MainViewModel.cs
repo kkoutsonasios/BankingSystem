089115624d79a7503d158988b01b57e780768a75
Kostas Koutsonasios <k_koutsonasios@live.com>
2016-09-19 19:41:42 +0300

    MVVM structure and some views

---
+++ BankingWindowsClient/BankingWindowsClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using BankingWindowsClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingWindowsClient.ViewModel
+{
+    class MainViewModel : BaseViewModel
+    {
+        public MainViewModel()
+        {
+            Main = new Main();
+        }
+
+        public Main Main {get;set;}
+
+
+
+    }
+}
